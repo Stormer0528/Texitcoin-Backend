052d11942a3c28428d927203a21555dd87768d78
vitalii <vitovodenko@gmail.com>
2024-06-19 11:25:36 -0400

    Reset migration on prisma

--- prisma/migrations/20240617085310_updated_all/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `userId` on table `sales` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "sales" ALTER COLUMN "userId" SET NOT NULL;
