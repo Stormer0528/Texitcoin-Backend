b8b79970b6fa7af7aeff0f83a827c158e24d3f99
vitalii <vitovodenko@gmail.com>
2024-06-20 11:43:44 -0400

    Replace consts from prisma to src

---
+++ src/consts/index.ts
@@ -0,0 +1,21 @@
+export const MEMBERS = ['james', 'CryptoCowboy', 'TXgoldrush', 'lissa', 'stepheb'];
+
+export const USERS = [
+  'ac2a2a5a-2ed5-11ef-a85b-00155de6f317',
+  'aca8a9d4-2ed5-11ef-82b6-00155de6f317',
+];
+
+export const STATISTICS = [
+  '34bcb1aa-2d8f-11ef-94f4-00155dcc9782',
+  '34f6ec8a-2d8f-11ef-8e57-00155dcc9782',
+  '352803ba-2d8f-11ef-81db-00155dcc9782',
+  '35479d24-2d8f-11ef-aa85-00155dcc9782',
+  'b92a55fe-2e7b-11ef-a50b-00155d2d52ac',
+];
+
+export const rpc_url = process.env.RPC_URL;
+export const rpc_username = process.env.RPC_USERNAME;
+export const rpc_password = process.env.RPC_PASSWORD;
+
+export const GET_BLOCK_COUNT = 'getblockcount';
+export const GET_DIFFICULTY = 'getdifficulty';
