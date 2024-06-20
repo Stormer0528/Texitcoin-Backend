c92da78e9da382bb1da7d8dc8f77ee111ffb804c
vitalii <vitovodenko@gmail.com>
2024-06-20 14:03:04 -0400

    Add type SaleReport, SaleReportInput, MineStatInput

--- src/type.ts
+++ src/type.ts
@@ -9,6 +9,31 @@ enum UserGroupRole {
   Owner = 'Owner',
 }
 
+export interface SaleReport {
+  hashPower: number;
+  amount: number;
+  member: Record<string, number>;
+}
+
+export interface SaleReportInput {
+  invoiceNo: number;
+  username: string;
+  mobile: string;
+  email: string;
+  productName: string;
+  date: Date;
+  paymentMethod: string;
+  amount: number;
+  hashPower: number;
+  assetId: string;
+}
+
+export interface MineStatInput {
+  date: Date;
+  totalBlocks: number;
+  newBlocks: number;
+}
+
 export interface UserGroupPermission {
   Report: UserGroupRole;
   Vendor: UserGroupRole;
