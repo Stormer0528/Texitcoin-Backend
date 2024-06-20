44df9cf7beae71457d5bf11fe66d4f889c0fdba5
vitalii <vitovodenko@gmail.com>
2024-06-20 13:58:15 -0400

    Add consts in prisma

---
+++ prisma/consts.ts
@@ -0,0 +1,21 @@
+export const USERS = [
+  '33c9f352-2d8f-11ef-8b6e-00155dcc9782',
+  '347d2058-2d8f-11ef-aabc-00155dcc9782',
+  '814f0350-2d9a-11ef-b9ee-00155de6f1f4',
+  '87f164dc-2d9a-11ef-8543-00155de6f1f4',
+  '8d81045c-2d9a-11ef-8ffa-00155de6f1f4',
+  '9203bc04-2d9a-11ef-9dbb-00155de6f1f4',
+  '97191716-2d9a-11ef-84d1-00155de6f1f4',
+  '9cf3f0e8-2d9a-11ef-8f81-00155de6f1f4',
+  'a239f4a8-2d9a-11ef-9419-00155de6f1f4',
+  'a669a1fe-2d9a-11ef-9bfa-00155de6f1f4',
+  'ace84c56-2d9a-11ef-8da6-00155de6f1f4',
+  'b19c2de4-2d9a-11ef-9057-00155de6f1f4',
+];
+
+export const STATISTICS = [
+  '34bcb1aa-2d8f-11ef-94f4-00155dcc9782',
+  '34f6ec8a-2d8f-11ef-8e57-00155dcc9782',
+  '352803ba-2d8f-11ef-81db-00155dcc9782',
+  '35479d24-2d8f-11ef-aa85-00155dcc9782',
+];
