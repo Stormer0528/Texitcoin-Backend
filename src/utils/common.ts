f3eeabc15d00c8f0d48e19ec604620dc9179f205
vitalii <vitovodenko@gmail.com>
2024-06-20 13:49:55 -0400

    Add formatDate utils

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,3 +1,9 @@
 export const toLocaleDate = function (date: Date, type: 'en-CA' | 'en-US' | 'en-GB'): string {
   return date.toLocaleDateString(type);
 };
+
+export const formatDate = function (date: Date): string {
+  const formattedDate = date.toISOString().split('T')[0];
+
+  return formattedDate;
+};
