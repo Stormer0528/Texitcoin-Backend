1de137a375c57b05bec0bb7cfce26da7e312674d
vitalii <vitovodenko@gmail.com>
2024-06-21 15:52:11 -0400

    Update seed

--- src/utils/getMlmData.ts
+++ src/utils/getMlmData.ts
@@ -1,16 +1,10 @@
-import { rpcCommand } from './rpcCommand';
 import { getMembers, getSales } from './connectMlm';
-import { GET_BLOCK_COUNT, GET_DIFFICULTY } from 'src/consts';
 import { processStatistics, processMemberStatistics } from './processData';
 
 const issuedAt = new Date();
 
 // get statistics from login, package and purchase_history on mlm
 export const getStatisticsFromMlm = async () => {
-  // const [newBlocks, difficulty] = await Promise.all([rpcComand(GET_BLOCK_COUNT), rpcCommand(GET_DIFFICULTY)]);
-  const newBlocks = 300;
-  const difficulty = 600;
-
   const sales = await getSales();
 
   const statistics = await processStatistics(sales);
@@ -27,14 +21,9 @@ export const getMemberFromMlm = async () => {
 
 // get member_statistics from login, package and puchase_history on mlm
 export const getMemberStatisticsFromMlm = async () => {
-  // const [newBlocks] = await Promise.all([rpcCommand('getblockcount')]);
-  const newBlocks = 300;
   const sales = await getSales();
 
-  const memberStatistics = await processMemberStatistics(sales, {
-    newBlocks,
-    issuedAt,
-  });
+  const memberStatistics = await processMemberStatistics(sales);
 
   return memberStatistics;
 };
