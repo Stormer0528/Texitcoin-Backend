bd7799582a40f4b2ef7a0bb0bc182e096144bb67
vitalii <vitovodenko@gmail.com>
2024-06-21 02:28:17 -0400

    Update Statistics Entity

--- src/entity/statistics/statistics.entity.ts
+++ src/entity/statistics/statistics.entity.ts
@@ -3,6 +3,7 @@ import { ObjectType, Field, ID } from 'type-graphql';
 import { BaseEntity } from '@/graphql/baseEntity';
 
 import { Sale } from '@/entity/sale/sale.entity';
+import { MemberStatistics } from '../memberStatistics/memberStatistics.entity';
 
 @ObjectType()
 export class Statistics extends BaseEntity {
@@ -31,8 +32,20 @@ export class Statistics extends BaseEntity {
   hashRate: number;
 
   @Field()
+  status: boolean = false;
+
+  @Field()
   issuedAt: Date;
+
+  @Field()
+  from: Date;
+
+  @Field()
+  to: Date;
 
   @Field(() => [Sale], { nullable: 'itemsAndList' })
   sales?: Sale[];
+
+  @Field(() => [MemberStatistics], { nullable: 'itemsAndList' })
+  memberStatistics?: MemberStatistics[];
 }
