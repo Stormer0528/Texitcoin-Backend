82197aeae2bc9a654458a4d30e4c779bd3565b3a
vitalii <vitovodenko@gmail.com>
2024-06-21 15:51:41 -0400

    Update logic in rewardTXC scripts

--- src/scripts/rewardTXC.ts
+++ src/scripts/rewardTXC.ts
@@ -1,18 +1,26 @@
 import { PrismaClient } from '@prisma/client';
 
-import { processStatistics } from '@/utils/processData';
-import { getSales } from '@/utils/connectMlm';
-import { formatDate } from '@/utils/common';
+import { processStatistics } from '../utils/processData';
+import { getSales } from '../utils/connectMlm';
+import { formatDate } from '../utils/common';
 
 const prisma = new PrismaClient();
 
 export const rewardTXC = async () => {
-  const { to, totalBlocks, totalHashPower } = await prisma.statistics.findFirst({
+  const result = await prisma.statistics.findFirst({
     orderBy: { to: 'desc' },
   });
 
+  const last = result
+    ? result
+    : {
+        to: new Date('2001-01-01'),
+        totalBlocks: 0,
+        totalHashPower: 0,
+      };
+
   const [data, sales] = await Promise.all([
-    prisma.block.findMany({ where: { createdAt: { gt: to } } }),
+    prisma.block.findMany({ where: { createdAt: { gt: last.to } } }),
     getSales(),
   ]);
 
@@ -20,13 +28,21 @@ export const rewardTXC = async () => {
 
   const statistics = await processStatistics(sales);
 
-  return {
-    ...statistics,
-    newBlocks,
-    totalBlocks: totalBlocks + newBlocks,
-    totalHashPower: totalHashPower + statistics.newHashPower,
-    issuedAt: formatDate(new Date()),
-    from: data[0].createdAt,
-    to: data[newBlocks].createdAt,
-  };
+  if (newBlocks !== 0) {
+    const result = await Promise.all([
+      prisma.statistics.create({
+        data: {
+          ...statistics,
+          newBlocks,
+          totalBlocks: last.totalBlocks + newBlocks,
+          totalHashPower: last.totalHashPower + statistics.newHashPower,
+          issuedAt: new Date(formatDate(new Date())),
+          from: newBlocks === 0 ? new Date() : data[0].createdAt,
+          to: newBlocks === 0 ? new Date() : data[newBlocks - 1].createdAt,
+        },
+      }),
+    ]);
+
+    return result;
+  }
 };
