d793c36ab89dfb1178df9e4116607afc07f653cd
vitalii <vitovodenko@gmail.com>
2024-06-21 10:57:46 -0400

    Update syncBlocks scripts

--- src/scripts/syncBlocks.ts
+++ src/scripts/syncBlocks.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 
-import { rpcCommand } from '@/utils/rpcCommand';
-import { GET_BLOCK_COUNT, GET_BLOCK_HASH, GET_BLOCK } from '@/consts';
+import { rpcCommand } from '../utils/rpcCommand';
+import { GET_BLOCK_COUNT, GET_BLOCK_HASH, GET_BLOCK, GET_NETWORK_HASH_PS } from '../consts';
 
 const prisma = new PrismaClient();
 
 const getBlockCountFromDatabase = async () => {
   try {
-    const { blockNo } = await prisma.block.findFirst({ orderBy: { createdAt: 'desc' } });
+    const block = await prisma.block.findFirst({ orderBy: { createdAt: 'desc' } });
 
-    return blockNo;
+    return block ? block.blockNo : 0;
   } catch (err) {
     console.log('error => ', err);
   }
@@ -19,7 +19,7 @@ const createBlocks = async (data) => {
   try {
     const result = await prisma.block.createMany({ data });
 
-    console.log('Created blocks');
+    console.log('Synced blocks');
 
     return result;
   } catch (err) {
@@ -28,25 +28,40 @@ const createBlocks = async (data) => {
 };
 
 export const syncBlocks = async () => {
-  console.log('Syncing block');
-  console.log('Connecting to PostgreSQL database');
+  try {
+    console.log('Syncing block');
+    console.log('Connecting to PostgreSQL database');
 
-  const [block = 0, count] = await Promise.all([
-    getBlockCountFromDatabase(),
-    rpcCommand({ method: GET_BLOCK_COUNT }),
-  ]);
+    const [block, count] = await Promise.all([
+      getBlockCountFromDatabase(),
+      rpcCommand({ method: GET_BLOCK_COUNT }),
+    ]);
 
-  const arr = Array.from({ length: count - block }, (_, index) => block + index + 1);
+    const arr = Array.from({ length: count - block }, (_, index) => block + index + 1);
 
-  const blocks = arr.map(async (item) => {
-    const [hash] = await Promise.all([rpcCommand({ method: GET_BLOCK_HASH, params: [item] })]);
+    const blocks = await Promise.all(
+      arr.map(async (item) => {
+        const [hash] = await Promise.all([rpcCommand({ method: GET_BLOCK_HASH, params: [item] })]);
 
-    const { height, time } = await rpcCommand({ method: GET_BLOCK, params: [hash] });
+        const [{ height, time, difficulty }] = await Promise.all([
+          await rpcCommand({
+            method: GET_BLOCK,
+            params: [hash],
+          }),
+        ]);
 
-    const createdAt = time.toLocaleString().toISOString().split('T')[0];
+        console.log('height => ', height);
 
-    return { blockNo: height, createdAt, updatedAt: createdAt };
-  });
+        const hashRate = await rpcCommand({ method: GET_NETWORK_HASH_PS, params: [120, height] });
 
-  await createBlocks(blocks);
+        const createdAt = new Date(time * 1000);
+
+        return { blockNo: height, difficulty, hashRate, createdAt, updatedAt: createdAt };
+      })
+    );
+
+    await createBlocks(blocks);
+  } catch (err) {
+    console.log('error => ', err);
+  }
 };
