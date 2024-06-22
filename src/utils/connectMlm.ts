2a3f66b6c2c42be055197bce5d6d332fccd11365
vitalii <vitovodenko@gmail.com>
2024-06-22 04:21:27 -0400

    Cleanup syncBlocks, connnectMlm utils

--- src/utils/connectMlm.ts
+++ src/utils/connectMlm.ts
@@ -2,7 +2,6 @@ import { SaleReportInput } from '@/type';
 import { createConnection, Connection } from 'mysql2/promise';
 import { PrismaClient } from '@prisma/client';
 import { CreateMemberInput } from '@/entity/member/member.type';
-import { isEmpty } from 'class-validator';
 
 const prisma = new PrismaClient();
 
@@ -27,7 +26,7 @@ export const getSales = async () => {
   const connection: Connection = await connectToDatabase();
 
   const [rows] = await connection.execute(
-    'SELECT txc_affiliate.mlm_purchase_history.invoice_no AS invoiceNo, txc_affiliate.mlm_login.username AS username, CONCAT("+", txc_affiliate.mlm_login.phone_code, " ", txc_affiliate.mlm_login.phone) AS mobile, txc_affiliate.mlm_login.email AS email, txc_affiliate.mlm_package.package AS productName, txc_affiliate.mlm_purchase_history.order_date AS issuedAt, txc_affiliate.mlm_purchase_history.payment_method AS paymentMethod, txc_affiliate.mlm_purchase_history.order_amount AS amount, txc_affiliate.mlm_purchase_history.hashpower AS hashPower, txc_affiliate.mlm_login.asset_id AS assetId FROM txc_affiliate.mlm_purchase_history LEFT JOIN txc_affiliate.mlm_login ON txc_affiliate.mlm_purchase_history.order_user_id = txc_affiliate.mlm_login.user_id LEFT JOIN  txc_affiliate.mlm_package ON txc_affiliate.mlm_purchase_history.order_product_id = txc_affiliate.mlm_package.package_id;'
+    'SELECT txc_affiliate.mlm_purchase_history.invoice_no AS invoiceNo, txc_affiliate.mlm_login.username AS username, CONCAT("+", txc_affiliate.mlm_login.phone_code, " ", txc_affiliate.mlm_login.phone) AS mobile, txc_affiliate.mlm_login.email AS email, txc_affiliate.mlm_package.package AS productName, txc_affiliate.mlm_purchase_history.order_date AS issuedAt, txc_affiliate.mlm_purchase_history.payment_method AS paymentMethod, txc_affiliate.mlm_purchase_history.order_amount AS amount, txc_affiliate.mlm_purchase_history.hashpower AS hashPower, txc_affiliate.mlm_login.asset_id AS assetId FROM txc_affiliate.mlm_purchase_history LEFT JOIN txc_affiliate.mlm_login ON txc_affiliate.mlm_purchase_history.order_user_id = txc_affiliate.mlm_login.user_id LEFT JOIN txc_affiliate.mlm_package ON txc_affiliate.mlm_purchase_history.order_product_id = txc_affiliate.mlm_package.package_id;'
   );
 
   const data: SaleReportInput[] = rows as SaleReportInput[];
