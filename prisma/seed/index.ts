f28ac23f6f25ca0e67eb1264f4e35d0079b42163
vitalii <vitovodenko@gmail.com>
2024-06-22 04:19:16 -0400

    Remove all data except for users seed

--- prisma/seed/index.ts
+++ prisma/seed/index.ts
@@ -1,59 +1,18 @@
 import { PrismaClient } from '@prisma/client';
 
-import { userData } from './user';
-import { getMemberFromMlm, getMemberStatisticsFromMlm } from '../../src/utils/getMlmData';
-import { getSales } from '../../src/utils/connectMlm';
-import { syncBlocks } from '../../src/scripts/syncBlocks';
-import { rewardTXC } from '../../src/scripts/rewardTXC';
+import { userData } from '../../prisma/seed/user';
 
 const prisma = new PrismaClient();
 
 async function main() {
   console.log(`Start seeding ...`);
-  console.log('creating block');
 
-  await syncBlocks();
-  await rewardTXC();
+  await prisma.user.createMany({ data: userData, skipDuplicates: true });
+
+  console.log('Finished seed');
 }
 
 main()
-  .then(async () => {
-    console.log('creating users');
-
-    await prisma.user.createMany({ data: userData, skipDuplicates: true });
-
-    const [members] = await Promise.all([getMemberFromMlm()]);
-
-    await Promise.all(
-      members.map(async (member) => {
-        const result = await prisma.member.upsert({
-          where: { userId: member.userId },
-          create: member,
-          update: member,
-        });
-
-        return result;
-      })
-    );
-  })
-  .then(async () => {
-    console.log('creating sales');
-
-    const [sales] = await Promise.all([getSales(), await prisma.sale.deleteMany()]);
-
-    await prisma.sale.createMany({ data: sales });
-
-    console.log(`Seeding finished.`);
-  })
-  .then(async () => {
-    console.log('creating memberStatistics');
-
-    const [memberStatistics] = await Promise.all([getMemberStatisticsFromMlm()]);
-
-    await prisma.memberStatistics.createMany({ data: memberStatistics });
-
-    console.log('Finished seed');
-  })
   .then(async () => {
     await prisma.$disconnect();
   })
