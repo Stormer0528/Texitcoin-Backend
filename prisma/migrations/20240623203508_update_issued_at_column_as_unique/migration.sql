9d61a1527a027db953123b5e8fbd82b929d6e5f5
vitalii <vitovodenko@gmail.com>
2024-06-23 17:06:25 -0400

    Remove unique from issuedAt in Statistics Schema

--- prisma/migrations/20240623203508_update_issued_at_column_as_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[issuedAt]` on the table `statistics` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "index_statistics_on_issuedAt" ON "statistics"("issuedAt");
