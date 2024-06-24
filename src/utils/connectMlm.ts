ad6c90d4ede7793b074cd73e53bfd23b0ad0deb4
vitalii <vitovodenko@gmail.com>
2024-06-24 10:45:41 -0400

    Add log info & Restructure rewardTXC script

--- src/utils/connectMlm.ts
+++ src/utils/connectMlm.ts
@@ -25,9 +25,13 @@ export const getStatistics = async () => {
 export const getSales = async () => {
   const connection: Connection = await connectToDatabase();
 
+  console.log('Connected to affiliate database to fetch sales');
+
   const [rows] = await connection.execute(
     'SELECT txc_affiliate.mlm_purchase_history.invoice_no AS invoiceNo, txc_affiliate.mlm_login.username AS username, CONCAT("+", txc_affiliate.mlm_login.phone_code, " ", txc_affiliate.mlm_login.phone) AS mobile, txc_affiliate.mlm_login.email AS email, txc_affiliate.mlm_package.package AS productName, txc_affiliate.mlm_purchase_history.order_date AS issuedAt, txc_affiliate.mlm_purchase_history.payment_method AS paymentMethod, txc_affiliate.mlm_purchase_history.order_amount AS amount, txc_affiliate.mlm_purchase_history.hashpower AS hashPower, txc_affiliate.mlm_login.asset_id AS assetId FROM txc_affiliate.mlm_purchase_history LEFT JOIN txc_affiliate.mlm_login ON txc_affiliate.mlm_purchase_history.order_user_id = txc_affiliate.mlm_login.user_id LEFT JOIN txc_affiliate.mlm_package ON txc_affiliate.mlm_purchase_history.order_product_id = txc_affiliate.mlm_package.package_id;'
   );
+
+  console.log('Fetched users info from affiliate');
 
   const data: SaleReportInput[] = rows as SaleReportInput[];
 
@@ -55,6 +59,8 @@ export const getSales = async () => {
 
 export const getMembers = async () => {
   const connection: Connection = await connectToDatabase();
+
+  console.log('Connected affiliate database to fetch members');
 
   const [rows] = await connection.execute(
     'SELECT username, CONCAT(first_name, " ", last_name) AS fullname, user_id AS userId, CONCAT("+", phone_code, " ", phone) AS mobile, email, password, primary_address AS address, asset_id AS assetId, TXCpayout AS txcPayout, blockio AS txcCold, join_date AS createdAt FROM txc_affiliate.mlm_login;'
