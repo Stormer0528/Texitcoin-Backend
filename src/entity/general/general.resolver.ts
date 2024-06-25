4e0e2f18ed7c7081e46ae84f0749b4c5d195cf4e
vitalii <vitovodenko@gmail.com>
2024-06-25 04:42:13 -0400

    Add meta for live stats

--- src/entity/general/general.resolver.ts
+++ src/entity/general/general.resolver.ts
@@ -25,13 +25,25 @@ export class GeneralResolver {
   @Query(() => EntityStats)
   async liveBlockStats(@Arg('data') { pastDays }: LiveStatsArgs): Promise<EntityStats> {
     const start = dayjs().subtract(pastDays, 'd').startOf('day').toDate();
-    const end = dayjs().endOf('day').toDate();
+    const end = dayjs().subtract(1, 'd').endOf('day').toDate();
 
-    const totalBlocks = await this.blockService.getBlocksCount({ where: {} });
-    const dailyBlockStats = await this.blockService.getBlocksCountByDate({
-      start,
-      end,
-    });
+    const now = new Date();
+    const past24Hours = dayjs().subtract(1, 'd').toDate();
+    const past48Hours = dayjs().subtract(2, 'd').toDate();
+
+    const [totalBlocks, dailyBlockStats, past24HoursBlocks, past48HoursBlocks] = await Promise.all([
+      this.blockService.getBlocksCount({ where: {} }),
+      this.blockService.getBlocksCountByDate({
+        start,
+        end,
+      }),
+      this.blockService.getBlocksCount({
+        where: { createdAt: { gt: past24Hours, lt: now } },
+      }),
+      this.blockService.getBlocksCount({
+        where: { createdAt: { gt: past48Hours, lt: past24Hours } },
+      }),
+    ]);
 
     return {
       total: totalBlocks,
@@ -39,7 +51,7 @@ export class GeneralResolver {
         field: dayjs(date).format('YYYY-MM-DD'),
         count,
       })),
-      meta: 1000,
+      meta: past24HoursBlocks - past48HoursBlocks,
     };
   }
 
@@ -48,13 +60,17 @@ export class GeneralResolver {
     const endOfToday = dayjs().endOf('day').toDate();
 
     const { to: lastRewardBlockTime } = await this.statisticsService.getLatestStatistics();
-    const totalNewBlocks = await this.blockService.getBlocksCount({
-      where: { createdAt: { gt: lastRewardBlockTime } },
-    });
-    const dailyNewBlockStats = await this.blockService.getTimeTookForBlock({
-      start: lastRewardBlockTime,
-      end: endOfToday,
-    });
+
+    const [totalNewBlocks, dailyNewBlockStats, latestBlock] = await Promise.all([
+      this.blockService.getBlocksCount({
+        where: { createdAt: { gt: lastRewardBlockTime } },
+      }),
+      this.blockService.getTimeTookForBlock({
+        start: lastRewardBlockTime,
+        end: endOfToday,
+      }),
+      this.blockService.getLatestBlock(),
+    ]);
 
     return {
       total: totalNewBlocks,
@@ -62,6 +78,7 @@ export class GeneralResolver {
         field: blockNo.toString(),
         count: timeTookInSeconds || 0,
       })),
+      meta: latestBlock.createdAt.valueOf() / 1000,
     };
   }
 
@@ -69,12 +86,18 @@ export class GeneralResolver {
   async liveUserStats(@Arg('data') { pastDays }: LiveStatsArgs): Promise<EntityStats> {
     const start = dayjs().subtract(pastDays, 'd').startOf('day').toDate();
     const end = dayjs().endOf('day').toDate();
+    const pastMonth = dayjs().subtract(1, 'M').toDate();
 
-    const totalMembers = await this.memberService.getMembersCount({ where: {} });
-    const dailyBlockStats = await this.memberService.getMembersCountByDate({
-      start,
-      end,
-    });
+    const [totalMembers, dailyBlockStats, pastMonthUsers] = await Promise.all([
+      this.memberService.getMembersCount({ where: {} }),
+      this.memberService.getMembersCountByDate({
+        start,
+        end,
+      }),
+      this.memberService.getMembersCount({
+        where: { createdAt: { gt: pastMonth } },
+      }),
+    ]);
 
     return {
       total: totalMembers,
@@ -82,7 +105,7 @@ export class GeneralResolver {
         field: dayjs(date).format('YYYY-MM-DD'),
         count,
       })),
-      meta: 1000,
+      meta: pastMonthUsers,
     };
   }
 }
