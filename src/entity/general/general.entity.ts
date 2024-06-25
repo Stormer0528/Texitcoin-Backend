c43708914f487b3f7c8c9ea725cf21d921704867
vitalii <vitovodenko@gmail.com>
2024-06-25 03:48:43 -0400

    Add general resolver

---
+++ src/entity/general/general.entity.ts
@@ -0,0 +1,22 @@
+import { ObjectType, Field, ID, Int } from 'type-graphql';
+
+@ObjectType()
+export class DailyStats {
+  @Field()
+  field: string;
+
+  @Field(() => Int)
+  count: number;
+}
+
+@ObjectType()
+export class EntityStats {
+  @Field()
+  total: number;
+
+  @Field(() => [DailyStats])
+  dailyData: DailyStats[];
+
+  @Field({ nullable: true })
+  meta?: number;
+}
