2319c5eb07ab0af9e0d06bde169390cfacdb1b0c
vitalii <vitovodenko@gmail.com>
2024-07-04 16:27:58 -0400

    Add field resolver in payouts & members

---
+++ src/graphql/loader/payoutLoader.ts
@@ -0,0 +1,28 @@
+import DataLoader from 'dataloader';
+
+import RootDataLoader from '.';
+import { Member } from '@prisma/client';
+
+export const membersForPayoutLoader = (parent: RootDataLoader) => {
+  return new DataLoader<string, Member[]>(
+    async (payoutIds: string[]) => {
+      const payoutsWithMembers = await parent.prisma.payout.findMany({
+        select: {
+          id: true,
+          members: true,
+        },
+        where: { id: { in: payoutIds } },
+      });
+
+      const payoutsWithMembersMap: Record<string, Member[]> = {};
+      payoutsWithMembers.forEach(({ id, members }) => {
+        payoutsWithMembersMap[id] = members;
+      });
+
+      return payoutIds.map((id) => payoutsWithMembersMap[id]);
+    },
+    {
+      ...parent.dataLoaderOptions,
+    }
+  );
+};
