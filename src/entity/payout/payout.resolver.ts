880147b1f20373082cb635f4ea717082c7353620
vitalii <vitovodenko@gmail.com>
2024-07-04 15:48:04 -0400

    Add payout resolver in payout

---
+++ src/entity/payout/payout.resolver.ts
@@ -0,0 +1,49 @@
+import { Service } from 'typedi';
+import { Arg, Args, Resolver, Query, Mutation, Info, Authorized } from 'type-graphql';
+import graphqlFields from 'graphql-fields';
+import { GraphQLResolveInfo } from 'graphql';
+
+import { UserRole } from '@/type';
+import { PayoutService } from './payout.service';
+import { CreatePayoutInput, PayoutQueryArgs, PayoutResponse } from './payout.type';
+import { Payout } from './payout.entity';
+
+@Service()
+@Resolver(() => Payout)
+export class PayoutResolver {
+  constructor(private readonly service: PayoutService) {}
+
+  @Query(() => PayoutResponse)
+  async payouts(
+    @Args() query: PayoutQueryArgs,
+    @Info() info: GraphQLResolveInfo
+  ): Promise<PayoutResponse> {
+    const fields = graphqlFields(info);
+
+    let promises: { total?: Promise<number>; payouts?: any } = {};
+
+    if ('total' in fields) {
+      promises.total = this.service.getPayoutsCount(query);
+    }
+
+    if ('payouts' in fields) {
+      promises.payouts = this.service.getPayouts(query);
+    }
+
+    const result = await Promise.all(Object.entries(promises));
+
+    let response: { total?: number; payouts?: Payout[] } = {};
+
+    for (let [key, value] of result) {
+      response[key] = value;
+    }
+
+    return response;
+  }
+
+  @Authorized([UserRole.Admin])
+  @Mutation(() => Payout)
+  async createPayout(@Arg('data') data: CreatePayoutInput): Promise<Payout> {
+    return this.service.createPayout(data);
+  }
+}
---
+++ src/entity/payout/payout.type.ts
@@ -0,0 +1,33 @@
+import type { Prisma } from '@prisma/client';
+import { ObjectType, InputType, Field, ArgsType } from 'type-graphql';
+
+import { PaginatedResponse } from '@/graphql/paginatedResponse';
+import { QueryArgsBase } from '@/graphql/queryArgs';
+import { Payout } from './payout.entity';
+
+// Payout Query Args
+@ArgsType()
+export class PayoutQueryArgs extends QueryArgsBase<Prisma.PayoutWhereInput> {}
+
+// Payout list response with pagination ( total )
+@ObjectType()
+export class PayoutResponse extends PaginatedResponse {
+  @Field(() => [Payout], { nullable: 'itemsAndList' })
+  payouts?: Payout[];
+}
+
+// Create Payout Input and Response
+@InputType()
+export class CreatePayoutInput {
+  @Field()
+  method: string;
+
+  @Field()
+  status: boolean;
+
+  @Field()
+  name: string;
+
+  @Field()
+  display: string;
+}
