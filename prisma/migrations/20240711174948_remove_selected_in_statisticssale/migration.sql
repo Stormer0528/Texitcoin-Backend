b5720bed3665b7ce5726c307e0797fee8ab08c81
vitalii <vitovodenko@gmail.com>
2024-07-11 13:53:22 -0400

    refactor createstatistic method in statistics resolver

---
+++ prisma/migrations/20240711174948_remove_selected_in_statisticssale/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `selected` on the `statistics_sale` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "statistics_sale" DROP COLUMN "selected";
