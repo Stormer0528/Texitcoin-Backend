86fd428641c888d268b51fdf7bc9c4a2665eed3a
vitalii <vitovodenko@gmail.com>
2024-07-23 17:01:14 -0400

    add refactorMember script

---
+++ src/scripts/refactorMember.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from '@prisma/client';
+
+import { getMembers } from '@/utils/connectMlm';
+import Bluebird from 'bluebird';
+import { PAYOUTS } from '@/consts';
+import { hashPassword } from '@/utils/auth';
+
+const prisma = new PrismaClient();
+
+const syncMembers = async () => {
+  try {
+    console.log('Syncing members...');
+
+    const mlmMembers = await getMembers();
+    const hashedPassword = await hashPassword('123456789');
+
+    const members = await Bluebird.map(
+      mlmMembers,
+      async (member) => {
+        const result = await prisma.member.upsert({
+          where: { userId: member.userId },
+          create: {
+            ...member,
+            payoutId: PAYOUTS[0],
+            password: hashedPassword,
+          },
+          update: {
+            ...member,
+            payoutId: PAYOUTS[0],
+          },
+        });
+
+        return result;
+      },
+      { concurrency: 10 }
+    );
+
+    console.log(`Successfully synced ${members.length} members`);
+
+    return members;
+  } catch (err) {
+    console.log('error => ', err);
+  }
+};
+
+async function refactorMember() {
+  const members = await syncMembers();
+}
+
+refactorMember();
