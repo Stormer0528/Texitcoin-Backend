a18df416dc6b01956ddd48f13e824c7356d74c19
vitalii <vitovodenko@gmail.com>
2024-09-10 05:16:17 -0400

    Update sync block script

--- src/scripts/syncBlocks.ts
+++ src/scripts/syncBlocks.ts
@@ -1,8 +1,9 @@
-import { PrismaClient } from '@prisma/client';
+import { Block, PrismaClient } from '@prisma/client';
 import Bluebird from 'bluebird';
 
 import { GET_BLOCK_COUNT, GET_BLOCK_HASH, GET_BLOCK, GET_NETWORK_HASH_PS } from '../consts';
 import { rpcCommand } from '../utils/rpcCommand';
+import dayjs from 'dayjs';
 
 const prisma = new PrismaClient();
 
@@ -16,9 +17,64 @@ const getBlockCountFromDatabase = async () => {
   }
 };
 
+const updateMonthlyWeeklyDailyBlocks = async (
+  dailyblockdata: Block[],
+  type: 'day' | 'week' | 'month'
+) => {
+  const hashRateMap: Record<string, number> = {};
+  const difficultyMap: Record<string, number> = {};
+  const countMap: Record<string, number> = {};
+  dailyblockdata.forEach((blockdata) => {
+    const startofType = dayjs(blockdata.issuedAt).startOf(type).format('YYYY-MM-DD');
+
+    if (!hashRateMap[startofType]) hashRateMap[startofType] = 0;
+    hashRateMap[startofType] += blockdata.hashRate;
+
+    if (!difficultyMap[startofType]) difficultyMap[startofType] = 0;
+    difficultyMap[startofType] += blockdata.difficulty;
+
+    if (!countMap[startofType]) countMap[startofType] = 0;
+    countMap[startofType]++;
+  });
+
+  await Bluebird.map(
+    Object.keys(countMap),
+    (date) => {
+      return prisma.weeklyBlock.upsert({
+        where: {
+          issuedAt: date,
+        },
+        create: {
+          hashRate: hashRateMap[date] / countMap[date],
+          difficulty: difficultyMap[date] / countMap[date],
+        },
+        update: {
+          hashRate: hashRateMap[date] / countMap[date],
+          difficulty: difficultyMap[date] / countMap[date],
+        },
+      });
+    },
+    {
+      concurrency: 10,
+    }
+  );
+};
+
 const createBlocks = async (data) => {
   try {
     const result = await prisma.block.createMany({ data });
+    const blockdates = [...new Set<string>(data.map((dt) => dt.issuedAt))];
+    const dailyblockdata = await prisma.block.findMany({
+      where: {
+        issuedAt: {
+          in: blockdates,
+        },
+      },
+    });
+
+    await updateMonthlyWeeklyDailyBlocks(dailyblockdata, 'day');
+    await updateMonthlyWeeklyDailyBlocks(dailyblockdata, 'week');
+    await updateMonthlyWeeklyDailyBlocks(dailyblockdata, 'month');
 
     console.log('Synced blocks');
 
