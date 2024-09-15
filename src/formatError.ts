123332ca5d7a3579a9a0d6101cfe6ab0ffaaf915
vitalii <vitovodenko@gmail.com>
2024-09-15 16:02:11 -0400

    Fix format error issue

--- src/formatError.ts
+++ src/formatError.ts
@@ -3,7 +3,10 @@ import { unwrapResolverError } from '@apollo/server/errors';
 import type { GraphQLFormattedError } from 'graphql';
 import { foreignKeyErrors, uniqueErrors } from './consts/errors';
 
-const checkUniqueError = (meta: Record<string, unknown>): GraphQLFormattedError => {
+const checkUniqueError = (
+  meta: Record<string, unknown>,
+  message: string
+): GraphQLFormattedError => {
   const { modelName, target } = meta;
   const sortedTarget = (target as string[]).sort();
   const error = uniqueErrors.find((err) => {
@@ -12,31 +15,42 @@ const checkUniqueError = (meta: Record<string, unknown>): GraphQLFormattedError 
       JSON.stringify(err.path.sort()) === JSON.stringify(sortedTarget)
     );
   });
-  return {
-    message: error.message,
-    path: error.path,
-  };
+  return error
+    ? {
+        message: error.message,
+        path: error.path,
+      }
+    : {
+        message,
+      };
 };
 
-const checkForeignKeyError = (meta: Record<string, unknown>): GraphQLFormattedError => {
+const checkForeignKeyError = (
+  meta: Record<string, unknown>,
+  message: string
+): GraphQLFormattedError => {
   const { field_name: fieldName } = meta;
   const slicedFieldName = (fieldName as string).slice(0, -8);
   const error = foreignKeyErrors.find((err) => {
     return err.constraintName === slicedFieldName;
   });
-  return {
-    message: error.message,
-    path: error.path,
-  };
+  return error
+    ? {
+        message: error.message,
+        path: error.path,
+      }
+    : {
+        message,
+      };
 };
 
 export const formatError = (formattedError: GraphQLFormattedError, error: unknown) => {
   const originalError = unwrapResolverError(error);
   if (originalError instanceof Prisma.PrismaClientKnownRequestError) {
     if (originalError.code === 'P2002') {
-      return checkUniqueError(originalError.meta);
+      return checkUniqueError(originalError.meta, formattedError.message);
     } else if (originalError.code === 'P2003') {
-      return checkForeignKeyError(originalError.meta);
+      return checkForeignKeyError(originalError.meta, formattedError.message);
     }
   }
   return {
