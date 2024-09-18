85be974bf6326d496a15e2ba01a936c44a129ec0
vitalii <vitovodenko@gmail.com>
2024-09-18 16:48:14 -0400

    Update logisze in logs

--- src/entity/member/member.resolver.ts
+++ src/entity/member/member.resolver.ts
@@ -399,7 +399,7 @@ export class MemberResolver {
   }
 
   @FieldResolver(() => [MemberLog])
-  async logs(@Root() member: Member, @Arg('logsize') logsize: number) {
+  async logs(@Root() member: Member, @Arg('logsize', { defaultValue: 10 }) logsize: number) {
     const logres = await this.elasticService.getLogByMinerUsername(member.username, logsize);
     return logres.hits.hits.map((hit) => ({
       id: hit._id,
